54007fad0791d89339bc3d5997ed6eb04b8dbe0f
JuanAn6 <juanantoniorizija666@gmail.com>
2024-02-25 10:19:59 +0100

    first comit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica Juan Antonio Garcia Jimenez</title>
+    <link rel="stylesheet" href="estil.css">
+</head>
+<body>
+    <main>
+        <?php include('./resources/sun.svg');?>
+        <?php include('./resources/tree.svg');?>
+        <?php include('./resources/tree2.svg');?>
+        <?php include('./resources/postlight.svg');?>
+        
+    </main>
+</body>
+</html>
+
+        
